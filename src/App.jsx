d353a1ddd87f5ae11f8b11035a89f43f4972b4df
jvazquez <gurneepeptides@gmail.com>
2025-10-06 19:27:58 -0500

    scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import CatalogPage from "./pages/CatalogPage.jsx";
-import ProductPage from "./pages/ProductPage.jsx";
+import CatalogPage from "./pages/CatalogPage";
+import ProductPage from "./pages/ProductPage";
+
+function TopBar() {
+  return (
+    <div className="topbar">
+      🚚 <strong>Same-Day Shipping</strong> on orders confirmed before 3PM CT •
+      For Research Use Only <br/> SITE IS UNDER CONSTRUCTION 
+    </div>
+  );
+}
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<CatalogPage />} />
-      <Route path="/product/:id" element={<ProductPage />} />
-    </Routes>
+    <>
+      <TopBar />
+      <Routes>
+        <Route path="/" element={<CatalogPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+      </Routes>
+    </>
   );
 }
